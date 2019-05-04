828827cfaf4039d2f7fd3be55326a873ac646842
alexbbb13 <alex_bbb@mail.ru>
2019-05-04 21:25:51 +0300

    Week 02 assignment

---
+++ web/W02/assign02.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" name="viewport" content="width=devicewidth, initial-scale=1.0">
+    <title>Alex B's Home Page</title>
+    <link rel = "stylesheet"
+          type = "text/css"
+          href = "w02style.css" />
+</head>
+<body>
+    <h2>Welcome to Alex B's Home Page</h2>
+    <hr>
+    <ul>
+        <li><a href="assign02.php">Home</a></li>
+        <li><a href="../assignments.html">Assignments</a></li>
+    </ul>
+    <img alt="Picture of A.B" width="30%", height="30%" src="me.jpg"  >
+
+            <p>My name is Alex B. I was born and live in <b>Moscow, Russia</b>. I am married, and we have have two boys age 8 and 2.</p>
+            <p> Last four years I am working as an Android developer <br>
+            I am trying to get a degree because I do not have formal education in the field.
+                I study in BYUI after gradiation from the <b>Pathway program</b>.</p>
+
+            <p>My major is <b>Software Engineering</b>, as I aim to get as much structured IT field knowledge as possible,
+                because I was a fan of computers and programming since childhood.
+            </p>
+    <div class="timestamp">
+        <?php
+            echo '<p>Created: '. date('Y-m-d') .'\n</p>';
+        ?>
+    </div>
+
+</body>
+</html>
