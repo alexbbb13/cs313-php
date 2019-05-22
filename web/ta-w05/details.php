5977730e2252847430c51b3a071aaf9abeccc4ae
alexbbb13 <alex_bbb@mail.ru>
2019-05-22 08:15:07 +0300

    fix15

--- web/ta-w05/details.php
+++ web/ta-w05/details.php
@@ -2,27 +2,27 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Team Work W 05 - Scripture details</title>
+    <title>Team Work W 05 details</title>
 </head>
 <body>
 <h2>Scripture details</h2>
 <br>
-<!--
+
 <?php
    require 'db.php';
    $db = getDb();
-	if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
         // retrieve the form data by using the element's name attributes value as key
         $scripture_id = $_GET['id'];
-        //$db = getDb();
         var_dump($db);
-        $r = selectById($db);
+        $allRows = selectById($db);
+        foreach($allRows as $r) 
         {
-					echo '<b>'.$r['book']." ".$r['chapter'].":".$r['verse'].'</b>';
-					echo ' <span class="text_content">'.$r['content'].'</span><br>';
-					echo '<br>';
-		}
+                    echo '<b>'.$r['book']." ".$r['chapter'].":".$r['verse'].'</b>';
+                    echo ' <span class="text_content">'.$r['content'].'</span><br>';
+                    echo '<br>';
+        }
+    }
 ?>
--->
 </body>
 </html>
