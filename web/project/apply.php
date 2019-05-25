9c0bafdbb63fdb28c84673ba9cb25db4f66b3dfc
alexbbb13 <alex_bbb@mail.ru>
2019-05-25 23:36:29 +0300

    project03

---
+++ web/project/apply.php
@@ -0,0 +1,22 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Apply for a job</title>
+     <link rel = "stylesheet"
+          type = "text/css"
+          href = "style.css" />
+</head>
+<body>
+<?php
+require 'navbar.php';
+?>	
+<h2>Under construction</h2>
+<br> 
+
+</body>
+</html>
