c6cad6f287510158939ff122533bb13ff673d586
alexbbb13 <alex_bbb@mail.ru>
2019-05-29 07:36:54 +0300

    WEEK06-TA

--- web/TA-W06/submitscripture.php
+++ web/TA-W06/submitscripture.php
@@ -11,6 +11,11 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 						$verse = $_GET['verse'];
 						$content = $_GET['content'];
 						$db = getDb();
+						var_dump($book);
+						var_dump($chapter);
+						var_dump($verse);
+						var_dump($content);	
+						die();
 					    $lastRow  = insertScripture($db, $book. $chapter, $verse, $content);
 					    var_dump($lastRow);	
 						foreach($_GET['check_list'] as $selected){
