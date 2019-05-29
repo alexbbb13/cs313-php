66cea0799bcfa83e1bd7cfc0967480136415938c
alexbbb13 <alex_bbb@mail.ru>
2019-05-29 08:23:12 +0300

    WEEK06-TA

--- web/TA-W06/submitscripture.php
+++ web/TA-W06/submitscripture.php
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 						    $allRows = selectAllScriptures($db);
 							foreach($allRows as $r) 
 							{
-								echo '<b>'.$r['book']." ".$r['chapter'].":".$r['verse'].$r['content'].'</b>';
+								echo '<b>'.$r['book']." ".$r['chapter'].":".$r['verse'].'<br>'.$r['content'].'</b>';
 								$id = $r['id'];
 								$topics = selectTopics($db, $id);
 
