efad136edd0ab4e4e2d6ebdeba4b68c3c8006965
Alex Burov <alex_bbb@mail.ru>
2019-06-07 16:02:09 +0300

    Added application details

--- web/project/applicationdetails.php
+++ web/project/applicationdetails.php
@@ -19,6 +19,17 @@ require 'navbar.php';
 
 <?php
 require 'db.php';
+
+/*
+jobs.id as jobId,
+  			applications.id as applicationId,
+  			jobs.user_id as clientUserId,
+  			applications.user_id as freelancerUserId,
+  			applications.freelance_service_id as freelancerServiceId,
+  			freelance_services.title,
+  			applications.rate_in_cents,
+  			applications.projected_hours
+*/
 //Local or Heroku
 function printRow($name, $value){
 	echo '<tr>';
@@ -26,32 +37,46 @@ function printRow($name, $value){
 	echo '<td>'.$value.'</td>';
 	echo ' </tr>';
 }
-
-function printTable($r, $id) {
-	echo'<h2>Freelance service:</h2><br>
+/*
+            jobs.id as jobId,
+  			applications.id as applicationId,
+  			jobs.user_id as clientUserId,
+  			applications.user_id as freelancerUserId,
+  			applications.freelance_service_id as freelancerServiceId,
+  			freelance_services.title,
+  			applications.rate_in_cents,
+  			applications.projected_hours,
+  			applications.cover_letter
+*/
+function printTableRow($r, $id) {
+	echo'<h2>Application details:</h2><br>
 	<table class="fancy">';
-	printRow('Name', $r['username']);
 	printRow('Title', $r['title']);
-	printRow('Description', $r['subtitle']);
-	printRow('Long description', $r['description']);
+	printRow('Cover Letter:', $r['cover_letter']);
 	$money  = $r['rate_in_cents']/100;
 	setlocale(LC_MONETARY, 'en_US');
 	printRow('Rate', money_format('%(#10n', $money));
+	printRow('Projected hours:', $r['projected_hours']);
 	echo '</table>';			
 }
 
 $db = getDb();
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
         // retrieve the form data by using the element's name attributes value as key
-           if (isset($_GET['id']))
+           if ( isset($_GET['application_id']) && 
+           	   isset($_GET['job_id']) && 
+           	   getSessionUser() != null 
+              )
 					{
-					    $filter = $_GET['id'];
-						$allRows = selectFreelanceById($db, $filter);	
+					    $applicationId = $_GET['application_id'];
+					    $jobId = $_GET['job_id'];
+					    $userId = getSessionUser();
+						$allRows = selectOneApplicationForMyJob($db, $jobId, $applicationId, $userId);
 					} else {
 					    echo '<b>Error!</b>';
 					}
 				if(sizeof($allRows) > 0) {
-					printTable($allRows[0], $filter);
+					printTableRow($allRows[0], $filter);
 				}
 
     } else {
