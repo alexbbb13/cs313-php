c00a7c5ef64a2a780c0f8ea8c31a9fa4a7463508
alexbbb13 <alex_bbb@mail.ru>
2019-06-08 10:41:12 +0300

    project01

--- web/project/navbar.php
+++ web/project/navbar.php
@@ -9,8 +9,9 @@ echo '<ul>
  	echo '<li><a href="./logout.php">Logout</a></li>';
  	echo '<li style="float:right"><a href="./freelance.php?my=true">'.getSessionUserName().'</a></li>';
  } else {
-   echo '<li><a href="./login.php">Login</a></li></ul><br>';
-   echo '<li><a href="./signup.php">Signup</a></li></ul><br>';
+   echo '<li><a href="./login.php">Login</a></li>';
+   echo '<li><a href="./signup.php">Signup</a></li>';
  }
+ echo '</ul><br>';
 
 ?>
